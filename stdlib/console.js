fbc4c954d10953ad6f8cb5488e0f494eb927c309
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-03-18 21:32:27 +0530

    console.js

---
+++ stdlib/console.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+var fs = require("fs");
+var input = require("input");
+let kk = fs.readFileSync(loc,
+            {encoding:'utf8', flag:'r'});
+ 
+// Display the file data
+// console.log(kk);
+//required string
+ kk = `var input = require('input');
+function requirejs (module,variable) {
+var variable = require(module);
+}
+
+ function log (argument) {
+ if (typeof (argument)== "string") {
+ console.log((argument).split('null').join('nulla'));
+  
+ } else {
+   console.log(argument);
+ }
+
+ }
+ `+ kk;
+
+//Interpretor
+//Cleaning string
+kk = kk.replace(/ +(?= )/g, '');
+
+function interpret(bhailang, js) {
+  kk = kk.split(bhailang).join(js);
+  // console.log(kk);
+} 
+
+
+// write loop here
+
+
+
+
+fs.writeFile("newfile.js", kk, function (err) {
+  if (err) throw err;
+  // console.log('File is created successfully.');
+  require("child_process").fork("newfile.js"); //change the path depending on where the file is.
+
+  //  const { fork } = require("child_process");
+
+  // const child = fork("newfile.js");
+
+  // console.log("main process is still running...");
+
+  // child.on("close", function (message) {
+  //   // console.log("child process exited with message " + message);
+  // });
+});
